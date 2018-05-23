abd36ae766ea3a775307cd702679f1a78fe6d512
tangxixaowen <846506584@qq.com>
2018-05-23 19:19:23 +0800

    添加 样式文件丢失

--- extend/Connect2-1/API/comm/inc.php
+++ extend/Connect2-1/API/comm/inc.php
@@ -1,2 +1,2 @@
 <?php die('forbidden'); ?>
-{"appid":"101393960","appkey":"ff93d6089f5c18f1f6885fcb068b4496","callback":"http://tangxiaowen.com/index.html","scope":"get_user_info,list_album,get_info","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
+{"appid":"101393960","appkey":"ff93d6089f5c18f1f6885fcb068b4496","callback":"http://tangxiaowen.com/xhome/index/index.html","scope":"get_user_info,add_album","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
